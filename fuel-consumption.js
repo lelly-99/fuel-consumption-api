b7a3547e626b77ec81f5ae431c6f4f5c97db659d
progress <leswifil@gmail.com>
2023-11-30 16:43:56 +0200

    fuel consmption

--- fuel-consumption.js
+++ fuel-consumption.js
@@ -1,74 +1,77 @@
 // vehicleService.js
 
 export default function FuelConsumption(db) {
-
-    async function addVehicle({ description, regNumber }) {
-
-        if (!description) {
-            return {
-                status: "error",
-                message: "description should not be blank"
-            }
-        }
-        if (!regNumber) {
-            return {
-                status: "error",
-                message: "regNumber should not be blank"
-            }
-        }
-        
-        // if (!regNumber && !description) {
-        //     return {
-        //         status: "error",
-        //         message: "description and regNumber should not be blank"
-        //     }
-        // }
-
-        const regex = /^(CA|CY|CF|CAA) \d{3}-\d{3}$/;
-        if (!regex.test(regNumber)) {
-            return {
-                status: "error",
-                message: "regNumber is invalid - should by CA, CY, CF, CAA followed by 3 numbers - 3 numbers"
-            }
-        }
-
-        const result = await db.oneOrNone(`insert into vehicles (description, reg_number) values ($1, $2) returning id`, [description, regNumber]);
-        return {
-            status: "success",
-            id: result.id
-        };
+  async function addVehicle({ description, regNumber }) {
+    if (!description) {
+      return {
+        status: "error",
+        message: "description should not be blank",
+      };
+    }
+    if (!regNumber) {
+      return {
+        status: "error",
+        message: "regNumber should not be blank",
+      };
     }
 
-    async function vehicles() {
-        return await db.manyOrNone(`select * from vehicles`);
+    // if (!regNumber && !description) {
+    //     return {
+    //         status: "error",
+    //         message: "description and regNumber should not be blank"
+    //     }
+    // }
+
+    const regex = /^(CA|CY|CF|CAA) \d{3}-\d{3}$/;
+    if (!regex.test(regNumber)) {
+      return {
+        status: "error",
+        message:
+          "regNumber is invalid - should by CA, CY, CF, CAA followed by 3 numbers - 3 numbers",
+      };
     }
 
-    async function vehicle(id) {
-        return await db.oneOrNone(`select * from vehicles where id = $1`, [id]);
+    const result = await db.oneOrNone(
+      `insert into vehicles (description, reg_number) values ($1, $2) returning id`,
+      [description, regNumber]
+    );
+    return {
+      status: "success",
+      id: result.id,
+    };
+  }
+
+  async function vehicles() {
+    return await db.manyOrNone(`select * from vehicles`);
+  }
+
+  async function vehicle(id) {
+    return await db.oneOrNone(`select * from vehicles where id = $1`, [id]);
+  }
+
+  async function refuel(vehicleId, liters, amount, distance, filled_up) {
+    //
+    if (!vehicleId) {
+      return {
+        status: "error",
+        message: "vehicleId not specified",
+      };
+    }
+    if (!liters) {
+      return {
+        status: "error",
+        message: "liters not specified",
+      };
+    }
+    if (!amount) {
+      return {
+        status: "error",
+        message: "amount not specified",
+      };
     }
 
-    async function refuel(vehicleId, liters, amount, distance, filled_up) {
-        // 
-        if (!vehicleId) {
-            return {
-                status: "error",
-                message: "vehicleId not specified"
-            }
-        }
-        if (!liters) {
-            return {
-                status: "error",
-                message: "liters not specified"
-            }
-        }
-        if (!amount) {
-            return {
-                status: "error",
-                message: "amount not specified"
-            }
-        }
-
-        const result = await db.oneOrNone(`
+    const result = await db.oneOrNone(
+      `
             insert into fuel_entries(
                 vehicle_id,
                 liters,
@@ -78,70 +81,73 @@ export default function FuelConsumption(db) {
             ) values (
                 $1, $2, $3, $4, $5
             ) returning id
-        `, [
-            vehicleId,
-            liters,
-            amount,
-            distance,
-            filled_up
-        ]);
+        `,
+      [vehicleId, liters, amount, distance, filled_up]
+    );
 
-        //
-        const results = await db.oneOrNone(`
+    //
+    const results = await db.oneOrNone(
+      `
             select sum(liters) as total_liters, sum(amount_paid) as total_amount 
             from fuel_entries 
-            where vehicle_id = $1`, [vehicleId])
+            where vehicle_id = $1`,
+      [vehicleId]
+    );
 
-        await db.none(`update vehicles set 
+    await db.none(
+      `update vehicles set 
             total_amount = $1, 
             total_liters = $2 where id = $3`,
-            [results.total_amount, results.total_liters, vehicleId])
+      [results.total_amount, results.total_liters, vehicleId]
+    );
 
-        // update fuel consumption
+    // update fuel consumption
+    //use the the added information to update : 
+    // Total Liters
+    // Total Amount
+    // Total Distance
 
-        // // (45690 - 45011) / (23 + 21)
-        const lastTwoEntriesForCar = await db.manyOrNone(`
+    // (45690 - 45011) / (23 + 21)
+    const lastTwoEntriesForCar = await db.manyOrNone(
+      `
             select * from fuel_entries 
             where vehicle_id = $1
-            order by id desc limit 2`, [vehicleId]);
+            order by id desc limit 2`,
+      [vehicleId]
+    );
 
-        let fuelConsumption = null;
+    let fuelConsumption = null;
 
+    if (lastTwoEntriesForCar.length == 2) {
+      const last = lastTwoEntriesForCar[0];
+      const previous = lastTwoEntriesForCar[1];
 
-        if (lastTwoEntriesForCar.length == 2) {
+      const fullTankRefuel = last.full_tank_refill && previous.full_tank_refill;
 
-            const last = lastTwoEntriesForCar[0];
-            const previous = lastTwoEntriesForCar[1];
+      if (fullTankRefuel) {
+        const distanceTraveled = last.distance - previous.distance;
+        const litersBought = Number(previous.liters) + Number(last.liters);
 
-            const fullTankRefuel = last.full_tank_refill && previous.full_tank_refill;
-
-            if (fullTankRefuel) {
-
-                const distanceTraveled = last.distance - previous.distance;
-                const litersBought = Number(previous.liters) + Number(last.liters);
-
-                fuelConsumption = (distanceTraveled) / litersBought;
-                fuelConsumption = fuelConsumption.toFixed(2);
-
-            }
-        }
-
-        await db.none(`update vehicles set fuel_consumption = $1 where id = $2`,
-            [fuelConsumption, vehicleId]);
-
-
-        return {
-            status: "success",
-            id: result.id
-        }
-
+        fuelConsumption = distanceTraveled / litersBought;
+        fuelConsumption = fuelConsumption.toFixed(2);
+      }
     }
+
+    await db.none(`update vehicles set fuel_consumption = $1 where id = $2`, [
+      fuelConsumption,
+      vehicleId,
+    ]);
 
     return {
-        addVehicle,
-        vehicle,
-        vehicles,
-        refuel
-    }
+      status: "success",
+      id: result.id,
+    };
+  }
 
+  return {
+    addVehicle,
+    vehicle,
+    vehicles,
+    refuel,
+  };
 }
